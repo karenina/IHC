45d1c2af25d3b23d225c593816f4f9abaa4755c3
sork30 <montiel_30@outlook.com>
2013-11-09 02:38:46 -0600

    CHEQUEN SI SUBI ALGO

---
+++ actividad2.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Aplicaciones web</title>
+<meta charset="utf-8">
+<meta name="description" content="Place your description here">
+<meta name="keywords" content="put, your, keyword, here">
+<meta name="author" content="Templates.com - website templates provider">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.4.2.min.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/Myriad_Pro_300.font.js"></script>
+<script type="text/javascript" src="js/Myriad_Pro_400.font.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<!--[if lt IE 7]>
+     <link rel="stylesheet" href="css/ie/ie6.css" type="text/css" media="screen">
+     <script type="text/javascript" src="js/ie_png.js"></script>
+     <script type="text/javascript">
+        ie_png.fix('.png, footer, header nav ul li a, .nav-bg, .list li img');
+     </script>
+<![endif]-->
+<!--[if lt IE 9]>
+  	<script type="text/javascript" src="js/html5.js"></script>
+  <![endif]-->
+</head>
+<body id="page1">
+<div class="wrap">
+   <!-- header -->
+   <header>
+      <div class="container">
+         <h1><a href="index.html">Aplicaciones Web</a></h1>
+         <nav>
+            <ul>
+               <li class="current"><a href="index.html" class="m1">Inicio</a></li>
+               <li><a href="about-us.html" class="m2">Acerca de Nosotros</a></li>
+               
+               <li><a href="contact-us.html" class="m4">Comentarios</a></li>
+               <li class="last"><a href="sitemap.html" class="m5">Mapa del sitio</a></li>
+            </ul>
+         </nav>
+       
+	   
+      </div>
+   </header>
+   <div class="container">
+      <!-- aside -->
+      <aside>
+         <h3>DreamWeaver</h3>
+         <ul class="categories">
+            <li><span><a href="activida1.php">Instalación y Conceptos Básicos</a></span></li>
+            <li><span><a href="#">Primeros pasos para el diseño Web</a></span>
+                  <ul>
+                      <li> <a href="actividad2.php">definiendo mi sitio</a></li>
+                       <li> <a href="#">configurar un sitio local</a></li>
+                  </ul> 
+            </li>
+            <li><span><a href="#">Manejo de Textos</a></span></li>
+           
+            
+         </ul>
+        
+         </aside>
+      <!-- content -->
+      <section id="content">
+         <div class="inside">
+            <h2>Definiendo <span>mi sitio</span></h2>
+                <font face="Arial, Helvetica, sans-serif" size="2"><b>Una 
+              cosa importante: </b>este es el paso más importante: setear,
+      dejar bien configurado en qué carpeta vamos a colocar TODO lo que vamos a
+      diseñar, y dejar así armado el sitio TAL CUAL lo vamos a tener en el FTP,
+      es decir, en el dominio que tengamos ya contratado para tener nuestra
+      página online. En resumen, les quiero decir que, si tenemos todo disperso
+      por cualquier parte de nuestro rígido, el mismo programa nos va a pedir
+      que coloquemos todo en un mismo lado.</font>
+      <p align="left"><font size="2" face="Arial, Helvetica, sans-serif">El desarrollo 
+        de un sitio Web con Dreamweaver comienza en la definici&oacute;n de una 
+        carpeta local para el sitio. En esta carpeta se guardar&aacute;n todos 
+        los archivos que lo conformen.<br>
+        Debemos hacer una carpeta en nuestro disco para cada sitio que deseemos 
+        crear. <br>
+        <br>
+        El paso siguiente es abrir el programa Dreamweaver. </font></p>
+      <p align="left"><font size="2" face="Arial, Helvetica, sans-serif">Abrimos
+      el programa y nos encontramos con dos ventanas (si tenemos las
+      preferencias por default) una hoja en blanco (listo para diseñar) y otra
+      que se denomina Site, es decir nos muestra todo lo que tenemos en nuestra
+      raíz. Elegimos Sitio/Nuevo Sitio (Site/New Site). Se abrirá un cuadro de
+      diálogo de definición de sitios. En listado Categorías (Categories),
+      seleccionamos Información Local (Local Info). En el campo Nombre del
+      Sitio (Site Name) escribimos un nombre para nuestro sitio. En Ruta de
+      Carpeta Local (Local Root Folder) debemos indicarle haciendo un click en
+      la carpeta amarilla, la ruta a la carpeta que predefinimos anteriormente
+      para nuestro sitio. </font></p>
+      <p align="center"><img src="images/imagen1.gif" tppabs="http://lawebdelprogramador.com/cursos/dream_4/imagenes/prefesite.gif" width="300" height="191" border="0"></p>
+      <p align="center">&nbsp;</p>
+      <p><font size="2" face="Arial, Helvetica, sans-serif">Una vez que navegamos 
+        hasta la carpeta hacemos un clic en Guardar (Save).<br>
+        Inmediatamente hacemos clic sobre el bot&oacute;n OK y el programa nos 
+        preguntar&aacute; si queremos que el programa haga un cach&eacute; de 
+        los archivos existentes para que actualice r&aacute;pidamente los enlaces 
+        cuando cambiamos, renombramos o borramos archivos.</font></p>
+      <p align="center"><font size="2" face="Arial, Helvetica, sans-serif"><img src="images/imagen2.gif" tppabs="http://lawebdelprogramador.com/cursos/dream_4/imagenes/site.gif" width="300" height="225" border="0"></font></p>
+      <p><font size="2" face="Arial, Helvetica, sans-serif">Seleccionamos Crear 
+        (Create).<br>
+        Ya hemos definido nuestro sitio y la ventana de sitio (Site Window) nos 
+        muestra que no tenemos todav&iacute;a ning&uacute;n archivo. Esta ventana 
+        nos permite administrar, borrar, copiar, pegar, mover y abrir los archivos 
+        que componen el sitio como si estuvi&eacute;ramos en el explorador de 
+        Windows. Tambi&eacute;n nos permite subir a la Web nuestros archivos.</font></p>
+              <!--mstheme--></font></td>
+      </tr>
+    </tbody>
+  </table><!--mstheme--><font face="Verdana, Arial, Helvetica">
+  </font></center>
+                 
+</center>&nbsp;<!--mstheme--></font><!--msnavigation--></td></tr><!--msnavigation--></table>
+               
+          
+         </div>
+      </section>
+   </div>
+</div>
+<!-- footer -->
+<footer>
+   <div class="container">
+      <div class="inside">
+                  <div class="wrapper">
+            <div class="aligncenter">Desarrollado por la  <a class="new_window" href="http://www.cs.buap.mx" target="_blank" rel="nofollow">Facultad de Ciencias de la Computacion</a><br/>
+               (c) 2013 <a class="new_window" href="http://www.cs.buap.mx" target="_blank" rel="nofollow">Centro de Tecnologias de Informacion | FCC BUAP</a></div>
+         
+         </div>
+      </div>
+   </div>
+</footer>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
---
+++ site/activida1.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Aplicaciones web</title>
+<meta charset="utf-8">
+<meta name="description" content="Place your description here">
+<meta name="keywords" content="put, your, keyword, here">
+<meta name="author" content="Templates.com - website templates provider">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.4.2.min.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/Myriad_Pro_300.font.js"></script>
+<script type="text/javascript" src="js/Myriad_Pro_400.font.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<!--[if lt IE 7]>
+     <link rel="stylesheet" href="css/ie/ie6.css" type="text/css" media="screen">
+     <script type="text/javascript" src="js/ie_png.js"></script>
+     <script type="text/javascript">
+        ie_png.fix('.png, footer, header nav ul li a, .nav-bg, .list li img');
+     </script>
+<![endif]-->
+<!--[if lt IE 9]>
+  	<script type="text/javascript" src="js/html5.js"></script>
+  <![endif]-->
+</head>
+<body id="page1">
+<div class="wrap">
+   <!-- header -->
+   <header>
+      <div class="container">
+         <h1><a href="index.html">Aplicaciones Web</a></h1>
+         <nav>
+            <ul>
+               <li class="current"><a href="index.html" class="m1">Inicio</a></li>
+               <li><a href="about-us.html" class="m2">Acerca de Nosotros</a></li>
+               
+               <li><a href="contact-us.html" class="m4">Comentarios</a></li>
+               <li class="last"><a href="sitemap.html" class="m5">Mapa del sitio</a></li>
+            </ul>
+         </nav>
+       
+	   
+      </div>
+   </header>
+   <div class="container">
+      <!-- aside -->
+      <aside>
+         <h3>DreamWeaver</h3>
+         <ul class="categories">
+            <li><span><a href="activida1.php">Instalación y Conceptos Básicos</a></span></li>
+            <li><span><a href="#">Primeros pasos para el diseño Web</a></span>
+                  <ul>
+                      <li> <a href="actividad2.php">definiendo mi sitio</a></li>
+                       <li> <a href="#">configurar un sitio local</a></li>
+                  </ul> 
+            </li>
+            <li><span><a href="#">Manejo de Textos</a></span></li>
+           
+            
+         </ul>
+        
+         </aside>
+      <!-- content -->
+      <section id="content">
+          <h2>Objetivo</h2>
+          <p>El objetivo de esta actividad es lograr una correcta instalación de Dreamweaver para poder usarlo en actividades posteriores.</p>
+          <h2>Instalación</h2>
+          <p>Antes de instalar Dreamweaver es necesario tener en cuenta los requisitos que necesita el sistema para su correcto funcionamiento:
+         <br> Windows<br>
+           Procesador Intel® Pentium® 4 o AMD Athlon® de 64 bits.<br>
+          <br> Microsoft® Windows® 7 con Service Pack 1, Windows 8 o Windows 8.1
+        1 GB de RAM.<br> 
+         <br> 1 GB de espacio disponible en el disco duro para la instalación; se requiere espacio libre adicional durante la instalación (no se puede instalar en dispositivos de almacenamiento flash extraíbles).<br> 
+        <br> Resolución de 1280 x 1024 con tarjeta de vídeo de 16 bits.<br> 
+         <br> Java™ Runtime Environment 1.6 (incluido).<br> 
+         <br> Se requiere disponer de una conexión a Internet y registrarse para la activación del software y la validación de los abonos obligatorias, y para el acceso a los servicios online*.
+
+</p>
+<p>Mac OS
+·         Procesador Intel multinúcleo.
+·         Mac OS X v10.7, v10.8, o v10.9
+·         1 GB de RAM.
+·         1 GB de espacio disponible en el disco duro para la instalación; se requiere espacio libre adicional durante la instalación (no se puede instalar en un volumen que utilice un sistema de distinción entre mayúsculas y minúsculas en archivos, ni en dispositivos de almacenamiento flash extraíbles).
+·         Resolución de 1280 x 1024 con tarjeta de vídeo de 16 bits.
+·         Java Runtime Environment 1.6.
+·         Software QuickTime 7.6.6 preciso para la reproducción de contenido multimedia HTML5.
+·         Se requiere disponer de una conexión a Internet y registrarse para la activación del software y la validación de los abonos obligatorias, y para el acceso a los servicios online*.
+</p>
+
+      </section>
+   </div>
+</div>
+<!-- footer -->
+<footer>
+   <div class="container">
+      <div class="inside">
+                  <div class="wrapper">
+            <div class="aligncenter">Desarrollado por la  <a class="new_window" href="http://www.cs.buap.mx" target="_blank" rel="nofollow">Facultad de Ciencias de la Computacion</a><br/>
+               (c) 2013 <a class="new_window" href="http://www.cs.buap.mx" target="_blank" rel="nofollow">Centro de Tecnologias de Informacion | FCC BUAP</a></div>
+         
+         </div>
+      </div>
+   </div>
+</footer>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
